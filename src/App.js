34ebb9ba9685191f6a3ceb18c8c67ce4394317ae
Aniket <duttaaniket005@gmail.com>
2024-08-12 17:08:36 +0530

    master: initial app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DevOps <code>CI/CD</code> tutorial.
         </p>
         <a
           className="App-link"
